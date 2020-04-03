dfc8e234f07931f7bec4cfc1ac71e902a823fd50
jshudd <jshudd@gmail.com>
2020-04-03 12:10:36 -0500

    Initial Code

---
+++ Circle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pi = 3.14;
+            var radius = 0;
+
+            Console.WriteLine("Input Circle Radius");
+            radius = Convert.ToInt32(Console.ReadLine());
+
+            var area = pi * (radius ^ 2);
+
+            Console.WriteLine($"The area of the circle is {area}");
+        }
+    }
+}
